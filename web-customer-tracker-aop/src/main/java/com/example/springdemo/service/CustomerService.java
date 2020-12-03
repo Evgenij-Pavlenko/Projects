eda48ff92fef0fa39946ab1a030ae1f60ab66d42
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-12-03 20:50:16 +0100

    add pom.xml - make als Maven-project - correct - all working in new project with AOP

---
+++ web-customer-tracker-aop/src/main/java/com/example/springdemo/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.example.springdemo.service;
+
+import com.example.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+
+    void saveCustomer(Customer theCustomer);
+
+    Customer getCustomer(int theId);
+
+    void deleteCustomer(int theId);
+
+    List<Customer> searchCustomers(String theSearchName);
+
+}
